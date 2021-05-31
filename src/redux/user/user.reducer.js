09da988658e82be2f3e72153a1f52e3aaf27b51b
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-05-31 16:51:15 -0400

    updated shop data with new data, added add to cart button, updated cart utils to group items in cart reducer

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,18 +1,18 @@
-import { UserActionTypes } from './user.types.js';
+import { UserActionTypes } from './user.types';
 
 const INITIAL_STATE = {
   currentUser: null
-}
+};
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  	case UserActionTypes.SET_CURRENT_USER:
-  	  return {
-  	  	...state,
-  	  	currentUser: action.payload
-  	  }
-  	default:
-  	  return state;
+    case UserActionTypes.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.payload
+      };
+    default:
+      return state;
   }
 };
 
