f0cacc242c7a8f7677996d183167ebc47cf9636a
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-06-08 15:01:02 -0400

    created user and cart sagas for signin, signup, and signin with google

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import {
   firestore,
@@ -26,6 +26,10 @@ export function* fetchCollections() {
   }
 }
 
-export function* onFetchCollectionsStart() {
+export function* fetchCollectionsStart() {
   yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollections);
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
